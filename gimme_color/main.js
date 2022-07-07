c443da83bd1745ad281333620f3edb36a0e1ca67
Newish0 <80089521+Newish0@users.noreply.github.com>
2022-07-07 15:52:37 -0700

    update gimme color

--- gimme_color/main.js
+++ gimme_color/main.js
@@ -4,34 +4,47 @@ if (location.protocol == 'http:') {
     location.replace(`https:${location.href.substring(location.protocol.length)}`);
 }
 
+const COLOR_NAMES = [];
+
+async function fetchColorNames() {
+    let resp = await fetch("data/colornames.min.json");
+    let data = await resp.json();
+    for (let color in data) {
+        COLOR_NAMES.push({
+            color: hexToRgb(color),
+            name: data[color]
+        });
+    }
+}
 
 
 // fetch database first
-let colorNames = {};
-fetch("data/colornames.min.json")
-    .then(response => response.json())
-    .then(data => colorNames = data);
+fetchColorNames();
 
 
+window.addEventListener("DOMContentLoaded", main);
 
-window.addEventListener("load", () => {
 
+async function main() {
 
     const FPS = 30;
     const CALCULATION_CYCLE_MULT = 0.5; // value b/w 0 - 1
     const DEFAULT_RESOLUTIONS = [240, 360, 480, 540, 720, 1080, 1440, 2560];
-
+    let curStream = null;
     let video = document.querySelector("#out-vid-container video");
     let canvas = document.querySelector("#canvas");
     let ctx = canvas.getContext("2d");
-    let frameCache = {
-        src: "",
-        img: new Image(),
-        update: null,
+
+    let cameraSettings = {
+        width: 720,
+        height: 720,
+        camID: undefined
     }
 
     let sampleSize = 64;
     let isPuased = false;
+
+    let nextCalTime = 0;
 
 
     // init video element
@@ -47,66 +60,105 @@ window.addEventListener("load", () => {
     });
 
     //init pause button
-    document.querySelector("#pause-sampling").addEventListener(("click"), (evt) => {
-        document.querySelector("#pause-sampling").querySelector("i").className = isPuased ? "fa-solid fa-pause" : "fa-solid fa-play";
-        frameCache.update();
-        isPuased = !isPuased;
+    document.querySelector("#pause-sampling").addEventListener(("click"), togglePause);
+
+    // check camera
+    let cameraPermissionStat = await navigator.permissions.query({ name: 'camera' });
+
+    if (cameraPermissionStat.state === "granted") {
+        // init camera list selector
+        initCameraList();
+    } else {
+        prompCameraRequired();
+    }
+
+    cameraPermissionStat.onchange = ((e) => {
+        // detecting if the event is a change
+        if (e.type === 'change') {
+            // checking what the new permissionStatus state is
+            const newState = e.target.state
+            if (newState === 'denied') {
+                prompCameraRequired();
+            } else if (newState === 'granted') {
+                initCameraList();
+            } else {
+                prompCameraRequired();
+            }
+        }
     });
 
 
-    // init camera list selector
-    initCameraList();
-
     // init camera select input listener
     document.querySelector("#camera-options").addEventListener("change", (evt) => {
-        initCamera(800, 800, evt.target.value);
+
+        cameraSettings.camID = evt.target.value;
+
+        
+        initCamera(cameraSettings).then(stream => curStream = stream);
+        // initCamera(cameraSettings);
+
+        if (isPuased) togglePause();
     });
 
     // init resolution select input listener 
     document.querySelector("#res-options").addEventListener("change", (evt) => {
-        initCamera(evt.target.value, evt.target.value, document.querySelector("#camera-options").value);
+
+
+        cameraSettings.width = evt.target.value;
+        cameraSettings.height = evt.target.value;
+
+        initCamera(cameraSettings).then(stream => curStream = stream);
+        // initCamera(cameraSettings);
+
+        if (isPuased) togglePause();
     });
 
 
-    initCamera();
+    curStream = await initCamera(cameraSettings);
 
-    video.addEventListener('play', function () {
-        let $this = this; //cache
-        let nextCalTime = 0;
+    loop();
 
-        frameCache.update = () => {
-            ctx.drawImage($this, 0, 0);
-            frameCache.src = canvas.toDataURL();
-            frameCache.img.src = frameCache.src;
+    function togglePause() {
+        togglePauseIcon();
+
+        if (isPuased) {
+            initCamera(cameraSettings);
+        } else {
+            curStream.getTracks().forEach(function (track) {
+                track.stop();
+            });
         }
 
-        (function loop() {
-            if (!$this.paused && !$this.ended) {
 
-                if (!isPuased) {
-                    ctx.drawImage($this, 0, 0);
-                } else {
-                    ctx.drawImage(frameCache.img, 0, 0);
-                }
+        isPuased = !isPuased;
+    }
 
-                let curTime = new Date().getTime();
-                if (curTime > nextCalTime) {
-                    let avgColor = getAverageRGB(ctx, canvas.width / 2 - sampleSize / 2, canvas.height / 2 - sampleSize / 2, sampleSize, sampleSize);
-                    updateInfoPanel(avgColor);
-                    nextCalTime = curTime + 1000 / FPS / CALCULATION_CYCLE_MULT;
-                } // if
+    function togglePauseIcon() {
+        document.querySelector("#pause-sampling").querySelector("i").className = isPuased ? "fa-solid fa-pause" : "fa-solid fa-play";
+    }
+
+    function loop() {
+        ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+        let curTime = performance.now();
+        if (curTime > nextCalTime) {
+            let avgColor = getAverageRGB(ctx, canvas.width / 2 - sampleSize / 2, canvas.height / 2 - sampleSize / 2, sampleSize, sampleSize);
+            updateInfoPanel(avgColor);
+            nextCalTime = curTime + 1000 / FPS / CALCULATION_CYCLE_MULT;
+        } // if
+
+        // draw GUI element after getAverageRGB calculation
+        ctx.strokeStyle = '#0f0';
+        ctx.lineWidth = canvas.width * 0.0025;
+        ctx.strokeRect(canvas.width / 2 - sampleSize / 2, canvas.height / 2 - sampleSize / 2, sampleSize, sampleSize);
+
+        setTimeout(loop, 1000 / FPS);
+    }
 
 
-                // draw GUI element after getAverageRGB calculation
-                ctx.strokeStyle = '#0f0';
-                ctx.lineWidth = canvas.width * 0.0025;
-                ctx.strokeRect(canvas.width / 2 - sampleSize / 2, canvas.height / 2 - sampleSize / 2, sampleSize, sampleSize);
-
-
-                setTimeout(loop, 1000 / FPS); // drawing at 30fps
-            }
-        })();
-    }, 0);
+    function prompCameraRequired() {
+        console.log("Camera is required!");
+    }
 
     function quickRefresh() {
         frameCache.update();
@@ -118,25 +170,20 @@ window.addEventListener("load", () => {
         }, 10000 / FPS)
     }
 
-    function initCameraList() {
-        navigator.mediaDevices.enumerateDevices()
-        .then(function (devices) {
-            let cameraOptions = document.querySelector("#camera-options");
+    async function initCameraList() {
+        let devices = await navigator.mediaDevices.enumerateDevices();
+        let cameraOptions = document.querySelector("#camera-options");
 
-            devices.forEach(function (device) {
-                if (device.kind == "videoinput") {
-                    let camOption = document.createElement("option");
+        for (const device of devices) {
+            if (device.kind == "videoinput") {
+                let camOption = document.createElement("option");
 
-                    camOption.value = device.deviceId;
-                    camOption.innerText = device.label;
+                camOption.value = device.deviceId;
+                camOption.innerText = device.label;
 
-                    cameraOptions.appendChild(camOption);
-                } // if
-            });
-        })
-        .catch(function (err) {
-            console.log(err.name + ": " + err.message);
-        });
+                cameraOptions.appendChild(camOption);
+            } // if
+        }
     }
 
     function initResolutionInput(res) {
@@ -152,7 +199,7 @@ window.addEventListener("load", () => {
             if (res.height >= DEFAULT_RESOLUTIONS[x] && res.width >= DEFAULT_RESOLUTIONS[x]) {
                 let option = document.createElement("option");
                 option.value = DEFAULT_RESOLUTIONS[x];
-                option.innerText = DEFAULT_RESOLUTIONS[x] + "x" + DEFAULT_RESOLUTIONS[x];
+                option.innerText = DEFAULT_RESOLUTIONS[x] + "p";
 
                 resOptions.appendChild(option);
                 allowedCount++;
@@ -165,89 +212,83 @@ window.addEventListener("load", () => {
     }
 
 
-    function initCamera(w, h, camID) {
+
+    async function initCamera(cameraSettings) {
         let videoParam = null;
 
-        if (camID != undefined) {
-            videoParam = {
-                width: w == undefined ? 800 : w,
-                height: h == undefined ? 800 : h,
-                facingMode: 'environment',
-                deviceId: { exact: camID }
-            }
-        } else {
-            videoParam = {
-                width: w == undefined ? 800 : w,
-                height: h == undefined ? 800 : h,
-                facingMode: 'environment'
-            }
+        // Close existing stream if defined
+        if (curStream) {
+            curStream.getTracks().forEach(function (track) {
+                track.stop();
+            });
         }
 
-        navigator.mediaDevices.getUserMedia({
+
+        
+
+        videoParam = {
+            width: cameraSettings.width,
+            height: cameraSettings.height,
+            facingMode: 'environment',
+            deviceId: { exact: cameraSettings.camID }
+        }
+
+        let stream = await navigator.mediaDevices.getUserMedia({
             video: videoParam,
             audio: false
-        })
-            .then(function (stream) {
-                video.srcObject = stream;
-                video.play();
+        });
 
-                let streamSettings = stream.getVideoTracks()[0].getSettings();
 
-                // TODO
-                initResolutionInput({
-                    width: 800,
-                    height: 800
-                });
+        video.srcObject = stream;
+        video.play();
 
-                document.querySelectorAll("#camera-options option").forEach((n) => {
-                    if (streamSettings.deviceId == n.value) {
-                        n.selected = true;
-                    } // if
-                });
+        let streamSettings = stream.getVideoTracks()[0].getSettings();
 
-                document.querySelectorAll("#res-options option").forEach((n) => {
-                    if (streamSettings.height == n.value) {
-                        n.selected = true;
-                    } // if
-                });
+        // TODO
+        initResolutionInput({
+            width: 800,
+            height: 800
+        });
 
-            })
-            .catch(function (err) {
-                console.log("An error occurred: " + err);
-            });
+        document.querySelectorAll("#camera-options option").forEach((n) => {
+            if (streamSettings.deviceId == n.value) {
+                n.selected = true;
+            } // if
+        });
 
+        document.querySelectorAll("#res-options option").forEach((n) => {
+            if (streamSettings.height == n.value) {
+                n.selected = true;
+            } // if
+        });
 
         // set canvas size = video size when known
         video.addEventListener('loadedmetadata', function () {
             canvas.width = video.videoWidth;
             canvas.height = video.videoHeight;
         });
+
+        return stream;
     } // initCamera
 
     function getClosestColor(colorHex) {
-        let last = {
-            color: Object.keys(colorNames)[0],
-            name: colorNames[Object.keys(colorNames)[0]]
-        };
+        let last = COLOR_NAMES[0];
 
         let target = hexToRgb(colorHex);
         let minDelta = 100000;
 
-        for (let x in colorNames) {
-            let cur = hexToRgb(x);
+        for (let i in COLOR_NAMES) {
+            let cur = COLOR_NAMES[i].color;
 
-            let dr = cur.r -  target.r;
-            let dg = cur.g -  target.g;
-            let db = cur.b -  target.b;
+            let dr = cur.r - target.r;
+            let dg = cur.g - target.g;
+            let db = cur.b - target.b;
 
             let delta = Math.sqrt(dr * dr + dg * dg + db * db);
 
             if (delta < minDelta) {
                 minDelta = delta;
-                last = {
-                    color: x,
-                    name: colorNames[x]
-                };
+                last = COLOR_NAMES[i];
             }
         }
 
@@ -273,27 +314,12 @@ window.addEventListener("load", () => {
         if (closestColor != undefined) {
 
             colorName.querySelector(".text-data").innerText = closestColor.name;
-            
+
             document.querySelectorAll(".name-color-preview").forEach((n) => {
                 n.style.backgroundColor = "#" + closestColor.color;
             })
         }
 
-    }
-
-    // https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-    function rgbToHex(r, g, b) {
-        return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
-    }
-
-    https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
-    function hexToRgb(hex) {
-        var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
-        return result ? {
-            r: parseInt(result[1], 16),
-            g: parseInt(result[2], 16),
-            b: parseInt(result[3], 16)
-        } : null;
     }
 
     https://stackoverflow.com/questions/2541481/get-average-color-of-image-via-javascript
@@ -327,41 +353,59 @@ window.addEventListener("load", () => {
 
     }
 
-    // https://stackoverflow.com/questions/8022885/rgb-to-hsv-color-in-javascript
-    function rgb2hsv(r, g, b) {
-        let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
-        rabs = r / 255;
-        gabs = g / 255;
-        babs = b / 255;
-        v = Math.max(rabs, gabs, babs),
-            diff = v - Math.min(rabs, gabs, babs);
-        diffc = c => (v - c) / 6 / diff + 1 / 2;
-        percentRoundFn = num => Math.round(num * 100) / 100;
-        if (diff == 0) {
-            h = s = 0;
-        } else {
-            s = diff / v;
-            rr = diffc(rabs);
-            gg = diffc(gabs);
-            bb = diffc(babs);
 
-            if (rabs === v) {
-                h = bb - gg;
-            } else if (gabs === v) {
-                h = (1 / 3) + rr - bb;
-            } else if (babs === v) {
-                h = (2 / 3) + gg - rr;
-            }
-            if (h < 0) {
-                h += 1;
-            } else if (h > 1) {
-                h -= 1;
-            }
+}
+
+
+// https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function rgbToHex(r, g, b) {
+    return ((1 << 24) + (r << 16) + (g << 8) + b).toString(16).slice(1);
+}
+
+https://stackoverflow.com/questions/5623838/rgb-to-hex-and-hex-to-rgb
+function hexToRgb(hex) {
+    var result = /^#?([a-f\d]{2})([a-f\d]{2})([a-f\d]{2})$/i.exec(hex);
+    return result ? {
+        r: parseInt(result[1], 16),
+        g: parseInt(result[2], 16),
+        b: parseInt(result[3], 16)
+    } : null;
+}
+
+// https://stackoverflow.com/questions/8022885/rgb-to-hsv-color-in-javascript
+function rgb2hsv(r, g, b) {
+    let rabs, gabs, babs, rr, gg, bb, h, s, v, diff, diffc, percentRoundFn;
+    rabs = r / 255;
+    gabs = g / 255;
+    babs = b / 255;
+    v = Math.max(rabs, gabs, babs),
+        diff = v - Math.min(rabs, gabs, babs);
+    diffc = c => (v - c) / 6 / diff + 1 / 2;
+    percentRoundFn = num => Math.round(num * 100) / 100;
+    if (diff == 0) {
+        h = s = 0;
+    } else {
+        s = diff / v;
+        rr = diffc(rabs);
+        gg = diffc(gabs);
+        bb = diffc(babs);
+
+        if (rabs === v) {
+            h = bb - gg;
+        } else if (gabs === v) {
+            h = (1 / 3) + rr - bb;
+        } else if (babs === v) {
+            h = (2 / 3) + gg - rr;
         }
-        return {
-            h: Math.round(h * 360),
-            s: percentRoundFn(s * 100),
-            v: percentRoundFn(v * 100)
-        };
+        if (h < 0) {
+            h += 1;
+        } else if (h > 1) {
+            h -= 1;
+        }
     }
-})
+    return {
+        h: Math.round(h * 360),
+        s: percentRoundFn(s * 100),
+        v: percentRoundFn(v * 100)
+    };
+}
